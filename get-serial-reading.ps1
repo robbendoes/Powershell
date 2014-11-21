2d4a8374fd52901dbfab9917b37b2f8fb43457e4
Rob de Jongh <rsdejongh@gmail.com>
2014-11-21 08:54:40 +0100

    Create get-serial-reading.ps1

---
+++ get-serial-reading.ps1
@@ -0,0 +1,43 @@
+﻿while ($i -le 7) 
+ {
+    $k = "4800","9600","38400"
+    foreach ( $ak in $k ) 
+    { 
+        write-host testing Baudrate $ak
+        write-host testing COM port $i
+        $port = New-Object System.IO.Ports.SerialPort
+        $port.PortName = "COM$i"
+        $port.BaudRate = "$ak"
+        $port.Parity = "None"
+        $port.DataBits = 8
+        $port.StopBits = 1
+        $port.ReadTimeout = 300 # 3 seconds
+        $port.DtrEnable = "true"
+
+        $port.open() #opens serial connection
+        Start-Sleep 1 # wait 2 seconds until Arduino is ready
+        try
+        {
+            $timeout = new-timespan -Seconds 15
+            $sw = [diagnostics.stopwatch]::StartNew()
+            while ($sw.elapsed -lt $timeout){
+                $myinput = $port.ReadLine()
+                $myinput
+                start-sleep -seconds 1
+                }
+            
+        }
+
+        catch [TimeoutException]
+        {
+        # Error handling code here
+        }
+
+        finally
+        {
+        # Any cleanup code goes here
+        } 
+        $port.Close() #closes serial connection
+    }
+ $i++
+}
