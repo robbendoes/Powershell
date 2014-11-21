87cb09092d86ad710f141badbec8051b329d8837
Rob de Jongh <rsdejongh@gmail.com>
2014-11-21 08:54:04 +0100

    Create get-comport-info.ps1

---
+++ get-comport-info.ps1
@@ -0,0 +1,15 @@
+﻿$target = Read-Host "Vul de inputmode in (ska)"
+
+if (Test-Connection -ComputerName $target -Count 2 -Quiet)
+    {
+        Copy-Item -force -verbose "D:\Powershell\Postgres_Script_DB\serial-portreading.ps1"  "\\$target\d$"
+        Write-Host $target "stopping geo" -ForegroundColor Gray               
+        invoke-command -ComputerName $target -ScriptBlock {  
+              Set-ExecutionPolicy unrestricted
+              get-service -name geoservice | stop-service -verbose -Force -ErrorAction SilentlyContinue
+              get-process winrun4j* | Stop-Process -force -verbose -ErrorAction SilentlyContinue
+              get-process java* | Stop-Process -force -verbose -ErrorAction SilentlyContinue
+              D:\serial-portreading.ps1   
+              } 
+
+    }
